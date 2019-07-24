b941790ce3c2d50e45c6fadc75440cb24a5a335b
ken <keng8684@outlook.es>
2019-07-23 20:21:15 -0500

    ref

---
+++ Dxzo.Lib/Dxzo.Data/Client/DataAccess.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Data;
+using System.Collections.Generic;
+
+namespace Dxzo.Data.Client
+{
+    public abstract class DataAccess : IDisposable
+    {
+        public ICollection<T> ExecuteQuery<T>(string storeProcedureName, IDictionary<string, object> parameters) where T : class, new()
+        {
+            var data = ExecuteQuery(storeProcedureName, parameters);
+
+            if (data != null)
+            {
+                if (!(data.Rows.Count > 0))
+                    return null;
+            } else
+                return null;
+
+            try
+            {
+                IList<T> tempList = new List<T>();
+
+                foreach (DataRow reg in data.Rows)
+                {
+                    T _object = new T();
+                    foreach (var property in _object.GetType().GetProperties())
+                    {
+                        try
+                        {
+                            var propertyInfo = _object.GetType().GetProperty(property.Name);
+                            propertyInfo.SetValue(_object, Convert.ChangeType(reg[property.Name], propertyInfo.PropertyType), null);
+                        }
+                        catch { continue; }
+                    }
+                    tempList.Add(_object);
+                }
+                return tempList;
+            }
+            catch
+            {
+                return null;
+            }
+        }
+        public abstract DataTable ExecuteQuery(string sentence);
+        public abstract DataTable ExecuteQuery(string storeProcedureName, IDictionary<string, object> parameters);
+        public abstract int ExecuteCommand(string storeProcedureName, IDictionary<string, object> parameters);
+        public abstract object ExecuteCommandScalar(string storeProcedureName, IDictionary<string, object> parameters);
+        public abstract void Dispose();
+    }
+}
